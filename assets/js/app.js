5c8984352821d214509086ec01c2097a1247e6a4
Mark Folmer Winther <mfw@ucn.dk>
2026-03-02 10:15:30 +0100

    Updated assignment

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+// Når du er færdig med denne opgave, ser siden ca sådan her ud: https://prnt.sc/AdvNjFr_TwHA
+
+// 1. Der skal, med brug af javascript, indsættes 1 skill mere, altså nedenstående html. Se mere i punkt 1.1 - 1.6 under
+/*
+    <div>
+        <span>CSS:</span>
+        <span>60%</span>
+        <meter min="0" max="100" value="60"></meter>
+    </div>
+*/
+
+// 1.1 Fang .mySkills med en egnet metode
+
+
+// 1.2 Anvend document.createElement() til at skabe en <div>, to <span> og et <meter> tag
+
+
+// 1.3 Anvend document.textContent til at indsætte data i 2*span(se ovenfor for indhold)
+
+// 1.4 Anvend .setAttribute() til at sætte de tre attributter i meter-tagget (min, max og value)
+
+
+// 1.5 Anvend .append() til at indsætte <div> i .mySkills
+
+
+// 1.6 Anvend .append() til at indsætte 2*span og 1*meter i <div>
+
+
+
+
+// 2. Anvend .cloneNode() til at klone den <li> med "Mit portfolio" og indsætte den to gange nedenfor (Ja, så kommer der til at stå "Mit portfolio" tre gange - pyt med det:) )
+
+// 2.1 Fang "mit portfolio"
+
+// 2.2 Indsæt tre kopier
+
+
+// 3. I footeren er der en div med class="oval". Anvend .style-objektet til at give den følgende:
+// width: 40px, height: 40px, background-color: pink, border-radius: 50%
+
+
+// 4. Se i dit .css dokument nederst at der er en class kaldet .niceStyling. Anvend .classList.add() til at sætte denne klasse på .oval2
